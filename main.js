c3c0f296bceddcc3d55668e9be2711fd2af96993
ljh <criya713@163.com>
2018-05-02 17:17:54 +0800

    顶部导航栏二级菜单debug

--- main.js
+++ main.js
@@ -6,11 +6,26 @@ window.onload = function () {
 };
 
 window.onscroll = function () {
-    console.log(window.scrollY)
     if(window.scrollY > 0){
         topNav.classList.add("stick");
     }
     else{
-        topNav.classList.remove("stick")
+        topNav.classList.remove("stick");
+    }
+}
+
+/*显示二级菜单*/
+let liTags = document.getElementsByClassName("submenuTrigger");
+for (let i=0; i<liTags.length; i++){
+    liTags[i].onmouseenter = function (e) {
+        let li = e.currentTarget;
+        let ol = li.getElementsByTagName('ol')[0];
+        ol.classList.add("active");
+    }
+
+    liTags[i].onmouseleave = function (e) {
+        let li = e.currentTarget;
+        let ol = li.getElementsByTagName('ol')[0];
+        ol.classList.remove("active");
     }
 }
