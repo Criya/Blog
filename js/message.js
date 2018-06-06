ecce307bcb991971ec88dcf29f6156157949cbc7
ljh <criya713@163.com>
2018-06-06 19:16:50 +0800

    添加留言板功能

---
+++ js/message.js
@@ -0,0 +1,95 @@
+!function () {
+    var view = document.querySelector("#siteMessage");
+
+    var model = {
+        init:function (data) {
+            AV.init(data)
+        },
+        get: function () {
+            var query = new AV.Query('Messages');
+            return query.find();
+        },
+        save: function (inputMessage) {
+            var Messages = AV.Object.extend('Messages');
+            var message = new Messages();
+            message.save({
+                name: inputMessage.name,
+                content: inputMessage.content,
+            })
+        }
+    }
+
+    var controller = {
+        view: null,
+        form: null,
+        model: null,
+        avInfo:{
+            appId: 'dBiiz9LeNIyQjCkyCCqbhF2e-gzGzoHsz',
+            appKey: 'uIh8KspCAegHJ78l58DpPzUV'
+        },
+        message: null,
+
+        init: function (view, model) {
+            this.view = view;
+            this.model = model;
+            this.form = view.querySelector("form")
+            this.model.init(this.avInfo);
+            this.bindEvent();
+            this.loadHistoryMessage();
+
+        },
+
+        bindEvent: function () {
+            var form = this.form;
+            form.addEventListener("submit", (e)=> {
+                e.preventDefault();
+                this.getInputMessage()
+                this.saveInputMessage();
+                this.reloadMessage();
+            })
+        },
+
+        getInputMessage: function () {
+            var content = this.form.querySelector("input[name=content]").value;
+            var name= this.form.querySelector("input[name=name]").value;
+            this.message = {
+                name: name,
+                content: content,
+            }
+        },
+
+        saveInputMessage: function () {
+            this.model.save(this.message)
+        },
+
+        reloadMessage: function () {
+            var message = this.message;
+            var ol = this.view.querySelector(".messageList");
+            var li = document.createElement("li");
+            li.innerHTML = `${message.name} : ${message.content}`;
+            ol.append(li);
+        },
+
+        loadHistoryMessage: function () {
+            this.model.get().then((m) => {
+                let array = m.map((item)=> item.attributes);
+                array.reverse().forEach((item)=>{
+                    var li = document.createElement("li");
+                    li.innerHTML = `${item.name} : ${item.content}`;
+                    this.view.querySelector(".messageList").append(li);
+                })
+            })
+        }
+
+    }
+
+    // var TestObject = AV.Object.extend('TestObject');
+    // var testObject = new TestObject();
+    // testObject.save({
+    //     words: 'Hello World!'
+    // }).then(function(object) {
+    //     alert('LeanCloud Rocks!');
+    // })
+
+    controller.init(view, model)
+}.call()
